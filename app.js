fa311496bbe638acf04f8c72f4a6772ef7278869
norentkhy <norent.dev@gmail.com>
2020-01-25 22:29:52 +0000

    added /418 path

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-
 	res.send('Hello Express World!');
+});
 
+app.get('/418', (req, res) => {
+	res.set('X-note', '418');
+	res.status(418);
+	res.send('418');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
